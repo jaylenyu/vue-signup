dea1db217d0c6c2338b48928c914755c5320d54d
Jaylen Yu <114912854+jaylenyu@users.noreply.github.com>
2023-10-26 23:59:58 +0900

    ADD: implement mainpage layout

--- app.vue
+++ app.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>
+  <div class="main-container">
     <NuxtPage />
   </div>
 </template>
+
+<style>
+.main-container {
+  min-height: 100vh;
+  padding: 100px;
+  background-color: #fafafa;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-    <button @click="goToSignUp">회원가입</button>
+  <div class="main-page">
+    <img class="main-image" src="https://static.balaan.co.kr/pc/legacy/img/banner/logo.png" alt="balaan" />
+    <button class="pl-button full large" @click="goToSignUp">회원가입</button>
   </div>
 </template>
 
@@ -13,3 +14,16 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.main-page {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.main-image {
+  margin-bottom: 100px;
+}
+</style>
