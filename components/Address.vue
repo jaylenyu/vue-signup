e3540c537f8891968c1d6339cd02c9b897bd6bdb
Jaylen Yu <114912854+jaylenyu@users.noreply.github.com>
2023-10-27 00:23:17 +0900

    MODIFY: Separate button functions and apply styles

--- components/Address.vue
+++ components/Address.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
     <h3>배송지 입력</h3>
+    <button class="pl-button full large" @click="$emit('prevStep')">이전</button>
+    <button class="pl-button full large" @click="$emit('nextStep')">다음</button>
   </div>
 </template>
 
--- components/Credit.vue
+++ components/Credit.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
     <h3>카드정보 입력</h3>
+    <button class="pl-button full large" @click="completeSignUp">회원가입 완료하기</button>
+
   </div>
 </template>
 
 <script>
 export default {
   methods: {
-    goToSignUpComplete() {
-      this.$router.push('/SignUpComplete')
+    completeSignUp() {
+      alert('회원가입이 완료되었습니다!');
+      this.$router.push('/signup/complete');
     }
   }
 }
