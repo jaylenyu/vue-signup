9712932eb9b6f303af9488583275cd2f181c49fa
Jaylen Yu <114912854+jaylenyu@users.noreply.github.com>
2023-10-28 15:10:29 +0900

    SETUP: piana setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ["@pinia/nuxt"],
   devtools: { enabled: true },
   ssr: false,
   css: ["plain-css", "~/assets/css/common.css"],
